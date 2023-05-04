f5330285de827e210f58f48eb3479b0c9f7f59f2
Simone Balbo <simone.balbo@ec.europa.eu>
2023-05-04 17:01:50 +0200

    add speedometer for soil water

---
+++ js/speedometer2.js
@@ -0,0 +1,90 @@
+Highcharts.chart('speedometer', {
+
+    chart: {
+        type: 'gauge',
+        plotBackgroundColor: null,
+        plotBackgroundImage: null,
+        plotBorderWidth: 0,
+        plotShadow: false,
+        height: '20%'
+    },
+
+    title: {
+        text: 'Soil water'
+    },
+
+    pane: {
+        startAngle: -90,
+        endAngle: 89.9,
+        background: null,
+        center: ['50%', '75%'],
+        size: '100%'
+    },
+
+    // the value axis
+    yAxis: {
+        min: 0,
+        max: 100,
+        tickPixelInterval: 72,
+        tickPosition: 'inside',
+        tickColor: Highcharts.defaultOptions.chart.backgroundColor || '#FFFFFF',
+        tickLength: 20,
+        tickWidth: 2,
+        minorTickInterval: null,
+        labels: {
+            distance: 20,
+            style: {
+                fontSize: '10px'
+            }
+        },
+        plotBands: [{
+            from: 0,
+            to: 73,
+            color: '#DF5353', //red
+            thickness: 20
+        }, {
+            from: 73,
+            to: 83,
+            color: '#DDDF0D', // yellow
+            thickness: 20
+        }, {
+            from: 83,
+            to: 100,
+            color: '#55BF3B', // green
+            thickness: 20
+        }]
+    },
+
+    series: [{
+        name: 'Soil water',
+        data: [87.0],
+        tooltip: {
+            valueSuffix: ' % of the global land surface'
+        },
+        dataLabels: {
+            format: 'Soil water content is normal over {y} % of global land surface',
+            borderWidth: 0,
+            color: (
+                Highcharts.defaultOptions.title &&
+                Highcharts.defaultOptions.title.style &&
+                Highcharts.defaultOptions.title.style.color
+            ) || '#333333',
+            style: {
+                fontSize: '10px'
+            }
+        },
+        dial: {
+            radius: '80%',
+            backgroundColor: 'gray',
+            baseWidth: 12,
+            baseLength: '0%',
+            rearLength: '0%'
+        },
+        pivot: {
+            backgroundColor: 'gray',
+            radius: 6
+        }
+
+    }]
+
+});
