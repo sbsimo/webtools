9973a88fdc0f2ec073429e0f2d67c391aa2068d0
BALBO Simone (JRC-ISPRA) <Simone.BALBO@ec.europa.eu>
2024-04-05 17:40:12 +0200

    update of gauges in LAI dashboard page

---
+++ js/lai_speedometer_szone.js
@@ -0,0 +1,110 @@
+Highcharts.chart('lai_speedometer_szone', {
+
+    chart: {
+        type: 'gauge',
+        plotBackgroundColor: null,
+        plotBackgroundImage: null,
+        plotBorderWidth: 0,
+        plotShadow: false,
+        height: '50%'
+    },
+
+    title: {
+        text: 'Southern zone - TODO'
+    },
+
+    pane: {
+        startAngle: -70,
+        endAngle: 70,
+        background: null,
+        center: ['50%', '75%'],
+        size: '100%'
+    },
+
+    // the value axis
+    yAxis: {
+        min: 0.560,
+        max: 0.604,
+        tickPixelInterval: 72,
+        tickPosition: 'inside',
+        tickColor: Highcharts.defaultOptions.chart.backgroundColor || '#FFFFFF',
+        tickLength: 20,
+        tickWidth: 2,
+        minorTickInterval: null,
+        labels: {
+            distance: 20,
+            style: {
+                fontSize: '10px'
+            }
+        },
+        plotBands: [{
+            from: 0.560, // avg - 3.5std
+            to: 0.563, // avg - 3std
+            color: '#fde725', //viridis scale 7 categories -> https://waldyrious.net/viridis-palette-generator/
+            thickness: 20
+        }, {
+            from: 0.563, // avg - 3std
+            to: 0.569, // avg - 2std
+            color: '#90d743',
+            thickness: 20
+        }, {
+            from: 0.569, // avg - 2std
+            to: 0.575, // avg - 1std
+            color: '#35b779',
+            thickness: 20
+        }, {
+            from: 0.575, // avg - 1std
+            to: 0.588, // avg + 1std
+            color: '#21918c',
+            thickness: 20
+        }, {
+            from: 0.588, // avg + 1std
+            to: 0.595, // avg + 2std
+            color: '#31688e',
+            thickness: 20
+        }, {
+            from: 0.595, // avg + 2std
+            to: 0.601, // avg + 3std
+            color: '#443983',
+            thickness: 20
+        }, {
+            from: 0.601, // avg + 3std
+            to: 0.604, // avg + 3.5std
+            color: '#440154',
+            thickness: 20
+        }]
+    },
+
+    series: [{
+        name: 'Average value',
+        data: [0.555],
+        tooltip: {
+            valueSuffix: ' '
+        },
+        dataLabels: {
+            format: 'average is {y}',
+            borderWidth: 0,
+            color: (
+                Highcharts.defaultOptions.title &&
+                Highcharts.defaultOptions.title.style &&
+                Highcharts.defaultOptions.title.style.color
+            ) || '#333333',
+            style: {
+                fontSize: '10px'
+            }
+        },
+        dial: {
+            radius: '80%',
+            backgroundColor: 'gray',
+            baseWidth: 12,
+            baseLength: '0%',
+            rearLength: '0%'
+        },
+        pivot: {
+            backgroundColor: 'gray',
+            radius: 6
+        }
+
+    }]
+
+});
