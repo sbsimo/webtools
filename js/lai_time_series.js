637c270953c62fe6ad06d1962343a25487ed9d76
Pere <peroc79@gmail.com>
2024-04-22 10:41:29 +0200

    Update lai_time_series.js

--- js/lai_time_series.js
+++ js/lai_time_series.js
@@ -1,94 +1,133 @@
 async function generateLineChart() {
 
-const response = await fetch('https://sbsimo.github.io/webtools/data/lai_global_average.csv');
-const data = await response.text();
+    const response = await fetch('https://sbsimo.github.io/webtools/data/lai_global_average.csv');
+    const data = await response.text();
 
-// Parse the .csv data
-const parsedData = Papa.parse(data, { header: true }).data;
-const averages = parsedData.map(row => Math.round(100*Number(row.corr_wavg))/100);
-const ranges = parsedData.map(row => [Math.round(100*Number(row['avg-1std']))/100, Math.round(100*Number(row['avg+1std']))/100]);
-const seriesMean = parsedData.map(row => Math.round(100*Number(row.pre21_mean))/100);
+    // Parse the .csv data
+    const parsedData = Papa.parse(data, { header: true }).data;
+    const averages = parsedData.map(row => Math.round(100 * Number(row.corr_wavg)) / 100);
+    const ranges = parsedData.map(row => [Math.round(100 * Number(row['avg-1std'])) / 100, Math.round(100 * Number(row['avg+1std'])) / 100]);
+    const seriesMean = parsedData.map(row => Math.round(100 * Number(row.pre21_mean)) / 100);
 
-Highcharts.chart('line-chart-container', {
+    var s = parsedData.map(row => row.year.substring(0, 4))
+    /* s.pop();
+    console.log(s) */
 
-    title: {
-        text: 'LAI global average',
-        align: 'center'
-    },
+    Highcharts.chart('line-chart-container', {
+        plotOptions: {
+            series: {
+                animation: {
+                    duration: 1550
+                }
+            }
+        },
 
-//    subtitle: {
-//        text: 'Source: ' +
-//            '<a href="https://www.yr.no/nb/historikk/graf/1-113585/Norge/Viken/Nesbyen/Nesbyen?q=2022-07"' +
-//            'target="_blank">YR</a>',
-//        align: 'left'
-//    },
-
-    xAxis: {
-//        type: 'datetime',
-        categories: parsedData.map(row => row.year.substring(0, 4))
-//        accessibility: {
-//            rangeDescription: 'Range: Jul 1st 2022 to Jul 31st 2022.'
-//        }
-    },
-
-    yAxis: {
         title: {
-            text: 'value'
-        }
-    },
+            text: 'LAI global average',
+            align: 'center',
+            style: {
+                color: '#a0b128',
+                fontSize: '1.5rem'
+            }
+        },
 
-    tooltip: {
-        crosshairs: true,
-        shared: true,
-        valueSuffix: '%'
-    },
+        //    subtitle: {
+        //        text: 'Source: ' +
+        //            '<a href="https://www.yr.no/nb/historikk/graf/1-113585/Norge/Viken/Nesbyen/Nesbyen?q=2022-07"' +
+        //            'target="_blank">YR</a>',
+        //        align: 'left'
+        //    },
 
-//    plotOptions: {
-//        series: {
-//            pointStart: Date.UTC(2022, 6, 1),
-//            pointIntervalUnit: 'day'
-//        }
-//    },
+        xAxis: {
+            /* min: 2014,*/ // Set minimum value for x-axis
+            //max: 2024, // Set maximum value for x-axis */
 
-    series: [{
-        name: 'Current-year global average',
-        data: averages,
-        zIndex: 1,
-        color: 'black',
-        marker: {
-            fillColor: 'white',
-            lineWidth: 2,
-//            lineColor: Highcharts.getOptions().colors[0]
-            lineColor: 'black'
-        }
-    }, {
-        name: '2014-2020 global average',
-        data: seriesMean,
-        zIndex: 0,
-        color: 'black',
-        dashStyle: 'shortdot',
-        fillOpacity: 0.7,
-        marker: {
-            fillColor: 'black',
-            lineWidth: 2,
-//            lineColor: Highcharts.getOptions().colors[0],
-            lineColor: 'black',
-            enabled: false
-        }
-    }, {
-        name: '1 stdev corridor',
-        data: ranges,
-        type: 'arearange',
-        lineWidth: 0,
-        linkedTo: ':previous',
-        color: '#21918c',
-        fillOpacity: 0.7,
-        zIndex: 0,
-        marker: {
-            enabled: false
-        }
-    }]
-});
+            //        type: 'datetime',
+            //categories: s.map(d => +d),
+            categories: parsedData.map(row => row.year.substring(0, 4)),
+            labels: {
+                style: {
+                    color: '#074007',
+                    fontSize: '.8rem'
+                }
+            }
+            //        accessibility: {
+            //            rangeDescription: 'Range: Jul 1st 2022 to Jul 31st 2022.'
+            //        }
+        },
+
+        yAxis: {
+            /* min: 1.45,
+            max: 1.55, */
+            title: {
+                text: 'value'
+            },
+            labels: {
+                style: {
+                    color: '#074007',
+                    fontSize: '.8rem'
+                }
+            }
+        },
+
+        tooltip: {
+            crosshairs: true,
+            shared: true,
+            valueSuffix: '%'
+        },
+
+        //    plotOptions: {
+        //        series: {
+        //            pointStart: Date.UTC(2022, 6, 1),
+        //            pointIntervalUnit: 'day'
+        //        }
+        //    },
+        legend: {
+            backgroundColor: '#FCFFC5'
+        },
+
+        series: [{
+            name: 'Current-year global average',
+            data: averages,
+            zIndex: 1,
+            color: '#a0b128',
+            marker: {
+                fillColor: 'white',
+                lineWidth: 2,
+                //            lineColor: Highcharts.getOptions().colors[0]
+                lineColor: 'black'
+            }
+        }, {
+            name: '2014-2020 global average',
+            data: seriesMean,
+            itemStyle: { 'font-size': '30px' },
+
+            zIndex: 0,
+            color: 'red',
+            dashStyle: 'shortdot',
+            fillOpacity: 1,
+
+            marker: {
+                fillColor: 'black',
+                lineWidth: 2,
+                //            lineColor: Highcharts.getOptions().colors[0],
+                lineColor: 'black',
+                enabled: false
+            }
+        }, {
+            name: '1 stdev corridor',
+            data: ranges,
+            type: 'arearange',
+            lineWidth: 0,
+            linkedTo: ':previous',
+            color: '#2aa6a1bf',
+            fillOpacity: 0.6,
+            zIndex: 0,
+            marker: {
+                enabled: false
+            }
+        }]
+    });
 
 }
 
