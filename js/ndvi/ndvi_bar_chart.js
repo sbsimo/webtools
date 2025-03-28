b4616533f8c1a4a55a3d19ef92ef393bcf4172a5
Simone BALBO <simone.balbo@ec.europa.eu>
2025-03-28 11:56:03 +0100

    add stratification according to land cover classes

--- js/ndvi/ndvi_bar_chart.js
+++ js/ndvi/ndvi_bar_chart.js
@@ -1,7 +1,7 @@
 var data = [
   {
-    x: ['forest', 'grassland', 'brush'],
-    y: [1.8, 1.2, -0.7],
+    x: ['Forest', 'Cropland', 'Herbaceous vegetation', 'Herbaceous wetland', 'Moss and lichen', 'Shrubs'],
+    y: [1.8, 1.4, 2.2, 2.5, 1.8, 2.1],
     type: 'bar'
   }
 ];
