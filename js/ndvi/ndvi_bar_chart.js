c3e9016eea76da90c7729a739c9887c9bde479ad
Simone BALBO <simone.balbo@ec.europa.eu>
2025-07-08 09:03:55 +0200

    change colors and title of bar chart (lcc)

--- js/ndvi/ndvi_bar_chart.js
+++ js/ndvi/ndvi_bar_chart.js
@@ -2,8 +2,18 @@ var data = [
   {
     x: ['Forest', 'Cropland', 'Herbaceous vegetation', 'Herbaceous wetland', 'Moss and lichen', 'Shrubs'],
     y: [1.8, 1.4, 2.2, 2.5, 1.8, 2.1],
-    type: 'bar'
+    type: 'bar',
+    marker: {
+      color: ['rgb(199,234,229)', 'rgb(199,234,229)', 'rgb(90,180,172)', 'rgb(90,180,172)',
+                'rgb(199,234,229)', 'rgb(90,180,172)']
+    }
   }
 ];
 
-Plotly.newPlot('ndvi_bar_chart', data);
+var layout = {
+    title: {
+      text: 'NDVI anomalies per vegetation cover class'
+    }
+}
+
+Plotly.newPlot('ndvi_bar_chart', data, layout);
