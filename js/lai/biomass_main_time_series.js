60bc1cb9d472e49a9c34779a84753bf0b178f07d
Simone BALBO <simone.balbo@ec.europa.eu>
2025-08-19 17:13:12 +0200

    add page for green biomass

---
+++ js/lai/biomass_main_time_series.js
@@ -0,0 +1,34 @@
+d3.csv("data/biomass/lai1km_global_wgt_avg_v4_CORR.csv", function(err, rows){
+function unpack(rows, key) {
+  return rows.map(function(row) {
+   return row[key];
+ });
+}
+
+var trace1 = {
+type: "scatter",
+mode: "lines",
+name: 'AAPL High',
+x: unpack(rows, 'datetime'),
+y: unpack(rows, 'LAI_corr3'),
+line: {color: '#17BECF'}
+}
+
+//var data = [trace1,trace2];
+var data = [trace1];
+
+var layout = {
+    title: {
+      text: 'LAI time series',
+      subtitle: {
+        text: 'global average values with 10-day steps'
+      }
+    },
+    xaxis: {
+        type: 'date',            // Required for date-based axis
+        dtick: 'M6'             // Gridline every 1 month
+    }
+};
+
+Plotly.newPlot("line-chart-container", data, layout);
+})
---
+++ js/lai/lai_bar_chart.js
@@ -0,0 +1,19 @@
+var data = [
+  {
+    x: ['Forest', 'Cropland', 'Herbaceous vegetation', 'Herbaceous wetland', 'Moss and lichen', 'Shrubs'],
+    y: [1.8, 1.4, 2.2, 2.5, 1.8, 2.1],
+    type: 'bar',
+    marker: {
+      color: ['rgb(199,234,229)', 'rgb(199,234,229)', 'rgb(90,180,172)', 'rgb(90,180,172)',
+                'rgb(199,234,229)', 'rgb(90,180,172)']
+    }
+  }
+];
+
+var layout = {
+    title: {
+      text: 'LAI anomalies per vegetation cover class'
+    }
+}
+
+Plotly.newPlot('lai_bar_chart', data, layout);
